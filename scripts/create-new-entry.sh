6cd32130a4eff0dc6ca9bfa1b9e3409de1990bf8
antonbriganti <anton.briganti@gmail.com>
2025-06-10 16:34:54 +1000

    update create script with new format

--- scripts/create-new-entry.sh
+++ scripts/create-new-entry.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # create new post
-file_number=$(($(ls md/| grep -E '^[0-9]+-' | sed -E 's/^([0-9]+)-.*/\1/' | sort -n | tail -n 1)+ 1))
 read -p "Enter mission (e.g. mission-01): " mission
 read -p "Enter article filename (e.g. session-01): " article
-mdfile="./md/0$file_number-$mission-$article.md"
+file_number=$(($(ls md/entry/$mission| grep -E '^[0-9]+-' | sed -E 's/^([0-9]+)-.*/\1/' | sort -n | tail -n 1)+ 1))
+mdfile="./md/entry/$mission/0$file_number-$article.md"
 cp ./_templates/template.md $mdfile
 
 
 # fill in info
 read -p "Enter title: " entrytitle
 sed -i "" "s/!!!TITLE!!!/$entrytitle/g" "$mdfile"
-echo "0. [$entrytitle](entry/$mission-$article)" >> ./md/index.md
+echo "0. [$entrytitle](entry/$mission/$article)" >> ./md/index.md
